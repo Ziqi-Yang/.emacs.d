be71eccea6279dc95aeafd99658e4fe2c698d26c
meowking <mr.meowking@posteo.com>
2025-02-02 08:59:15 +0800

    update

--- init.el
+++ init.el
@@ -6,9 +6,9 @@
 ;; (setq debug-on-error t)
 
 ;; https://github.com/progfolio/elpaca/wiki/Warnings-and-Errors#unable-to-determine-elpaca-core-date
-(defvar elpaca-core-date '(20250106))
+(defvar elpaca-core-date '(20250202))
 
-(defvar elpaca-installer-version 0.8)
+(defvar elpaca-installer-version 0.9)
 (defvar elpaca-directory (expand-file-name ".local/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
--- init/init-ui.el
+++ init/init-ui.el
@@ -147,7 +147,7 @@
   "Setup Fonts."
   (let ((default-font "Iosevka")  ; Iosevka, IBM Plex Mono, Cascadia Code
         (CJK-font "LXGW Neo XiHei")
-        (font-size 22))
+        (font-size 20))
     (when (display-graphic-p)
       (when (member default-font (font-family-list))
         ;; @ default font
