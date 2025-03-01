c725527b8c22ff2b21dff27f0bd20618308f23fb
meowking <mr.meowking@posteo.com>
2025-03-01 12:25:46 +0800

    update

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; (setq debug-on-error t)
 
 ;; https://github.com/progfolio/elpaca/wiki/Warnings-and-Errors#unable-to-determine-elpaca-core-date
-(defvar elpaca-core-date '(20250202))
+(defvar elpaca-core-date '(20250301))
 
 (defvar elpaca-installer-version 0.9)
 (defvar elpaca-directory (expand-file-name ".local/elpaca/" user-emacs-directory))
